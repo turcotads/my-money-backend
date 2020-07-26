7c3f3d54c3b146761b8322b570a267ea9171c627
Fábio Madeira Peres <fabinhomadeira@gmail.com>
2020-07-26 10:40:53 -0300

    Summary credits and debts

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
-module.exports = mongoose.connect("mongodb://localhost:27017/mymoney");
+module.exports = mongoose.connect("mongodb://localhost:27017/mymoney", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório";
 mongoose.Error.messages.Number.min =
